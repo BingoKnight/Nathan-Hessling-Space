08b1f57b36eb695ca48a606d48b4efae81e234b0
Nathan Hessling <nrhessling@gmail.com>
2020-12-13 17:20:51 -0500

    adding images for tech list and removing unordered list

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,42 +8,42 @@ export default () => {
     const techCardList = [
         {
             title: 'React',
-            image: '/static/images/react_logo.png',
+            image: '/images/react_logo.png',
             url: 'https://reactjs.org/'
         },
         {
             title: 'Gatsby',
-            image: '/static/images/gatsby_logo.png',
+            image: '/images/gatsby_logo.png',
             url: 'https://www.gatsbyjs.com/'
         },
         {
             title: 'Django',
-            image: '/static/images/django_logo.png',
+            image: '/images/django_logo.png',
             url: 'https://www.djangoproject.com/'
         },
         {
             title: 'Elastic Beanstalk',
-            image: '/static/images/eb_logo.png',
+            image: '/images/eb_logo.png',
             url: 'https://aws.amazon.com/elasticbeanstalk/'
         },
         {
             title: 'CloudFront',
-            image: '/static/images/cloudfront_logo.png',
+            image: '/images/cloudfront_logo.png',
             url: 'https://aws.amazon.com/cloudfront/'
         },
         {
             title: 'S3',
-            image: '/static/images/s3_logo.png',
+            image: '/images/s3_logo.png',
             url: 'https://aws.amazon.com/s3/'
         },
         {
             title: 'Route53',
-            image: '/static/images/route53_logo.png',
+            image: '/images/route53_logo.png',
             url: 'https://aws.amazon.com/route53/'
         },
         {
             title: 'EC2',
-            image: '/static/images/ec2_logo.png',
+            image: '/images/ec2_logo.png',
             url: 'https://aws.amazon.com/ec2/'
         }
     ]
@@ -63,33 +63,17 @@ export default () => {
                 turned into the desire to create something which includes it. This website is completely from scratch in
                 respect to website builders (eg. Wix, Wordpress, GoDaddy, etc) using the technologies below:
 
-                <ul className={'content-list'}>
-                    <li><a href={'https://reactjs.org/'}>React</a></li>
-                    <li><a href={'https://www.gatsbyjs.com/'}>Gatsby</a></li>
-                    <li><a href={'https://www.djangoproject.com/'}>Django</a></li>
-                    <li>
-                        <a href={'https://aws.amazon.com/'}>AWS</a>
-                        <ul>
-                            <li><a href={'https://aws.amazon.com/elasticbeanstalk/'}>Elastic Beanstalk</a></li>
-                            <li><a href={'https://aws.amazon.com/cloudfront/'}>CloudFront</a></li>
-                            <li><a href={'https://aws.amazon.com/s3/'}>S3</a></li>
-                            <li><a href={'https://aws.amazon.com/route53/'}>Route53</a></li>
-                            <li><a href={'https://aws.amazon.com/ec2/'}>EC2</a></li>
-                        </ul>
-                    </li>
 
-                </ul>
-
-                <CardContainer>
-                    {
-                        techCardList.map(card =>
-                            <Card backgroundImage={card.image} url={card.url}>
-                                {card.title}
-                            </Card>
-                        )
-                    }
-                </CardContainer>
             </p>
+            <CardContainer>
+                {
+                    techCardList.map(card =>
+                        <Card backgroundImage={card.image} url={card.url}>
+                            {card.title}
+                        </Card>
+                    )
+                }
+            </CardContainer>
         </Layout>
     )
 }
